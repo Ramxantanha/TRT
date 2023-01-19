b9170678fd6be9be38c31333f6028c7eab9b4605
Ramxantanha <115856285+Ramxantanha@users.noreply.github.com>
2023-01-19 17:55:17 +0500

    Update trt.py

--- trt.py
+++ trt.py
@@ -1,4 +1,4 @@
-import os,time,platform
+Jimport os,time,platform
 os.system('clear')
 print('[•] Checking Updates...')
 os.system('git pull')
@@ -12,7 +12,7 @@ if bit=='64bit':
     print(f'{red}[•] Join Over Facebook Group {white}')
     os.system('xdg-open https://facebook.com/groups/412423917720633//')
     time.sleep(0.05)
-    import trt2
+    import trt1
 elif bit=='32bit':
     import trt32
 else:
