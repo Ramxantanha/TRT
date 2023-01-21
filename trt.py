2ec944c70435aaf23f4c564aafcd6b2c4d29bc08
Ramxantanha <115856285+Ramxantanha@users.noreply.github.com>
2023-01-21 12:42:47 +0500

    Add files via upload

---
+++ trt.py
@@ -0,0 +1,19 @@
+import os,time,platform
+os.system('clear')
+print('[•] Checking Updates...')
+os.system('git pull')
+green = ('\033[1;32m')
+white = ('\033[1;37m')
+red = ('\033[1;31m')
+
+print('<------------------------------------>')
+bit = platform.architecture()[0]
+if bit=='64bit':
+    print(f'{red}[•] Join Over Facebook Group {white}')
+    os.system('xdg-open https://facebook.com/groups/412423917720633//')
+    time.sleep(0.05)
+    import trt1
+elif bit=='32bit':
+    import trt32
+else:
+    print(f'{green}[×] Sorry System Not Support{white}')
