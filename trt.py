3a9af46db03c750a958df314210a872f028c95b6
Ramxantanha <115856285+Ramxantanha@users.noreply.github.com>
2023-02-10 14:52:40 +0500

    Update trt.py

--- trt.py
+++ trt.py
@@ -3,6 +3,6 @@ os.system('git pull')
  
 trt=platform.architecture()[0]
 if trt=="32bit":
-    print('Sorry 32 Bit Not Saported...')
+    print('Sorry 32 Bit Not Supported...')
 elif trt=="64bit":
     __import__("trt1")
