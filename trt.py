e49eb3d5a7fc5dbd1cf70df756e40a5473ae23f8
Ramxantanha <115856285+Ramxantanha@users.noreply.github.com>
2023-02-16 12:33:34 +0500

    Delete trt.py

--- trt.py
+++
@@ -1,8 +0,0 @@
-import os,platform
-os.system('git pull')
- 
-trt=platform.architecture()[0]
-if trt=="32bit":
-    print('Sorry 32 Bit Not Supported...')
-elif trt=="64bit":
-    __import__("trt1")
