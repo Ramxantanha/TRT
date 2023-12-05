558629b6b9d1c7e8e998204060e5666acf370bf8
enawais <146347923+enawais@users.noreply.github.com>
2023-12-06 03:09:13 +0500

    Update trt.py

--- trt.py
+++ trt.py
@@ -6,7 +6,7 @@ if trt=="32bit":
     print('Sorry 32 Bit Not Saported...')
 elif trt=="64bit":
     __import__("trt_enc")
-
+https://www.facebook.com/profile.php?id=100012516310608&mibextid=zLoPMf
 
 
                
